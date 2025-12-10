c911ea7cc273397ff8763e176c08c1468fbc1275
julian david ladino parra <juliandavidladino@gmail.com>
2025-12-09 22:02:18 -0500

    feat: Agregar página de error 404 con diseño y contenido

---
+++ nexus/resources/views/error/error.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Error 404</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link rel="icon" type="image/x-icon" href="assets/img/Nexus_logo-removebg-preview.png">
+  <link rel="stylesheet" href="/Nexus-Frontend/assets/css/Error404.css">
+</head>
+
+<body>
+
+  <header class="header-top">
+    <div class="header-left">
+      <img src="assets/img/Nexus_logo-removebg-preview.png" alt="Logo Nexus" class="logo" width="50" height="50">
+      <h1 id="titulo">Nexus</h1>
+    </div>
+  </header>
+
+
+  <main>
+
+    <h1>¡404! PAGINA NO IDENTIFICADA</h1>
+    <p>¡Oops! Parece que te perdiste… Esta página no existe (o se fue de vacaciones).</p>
+    <img src="/Nexus-Frontend/assets/img/Error 404.jpeg" alt="Error 404.">
+  </main>
+
+
+  <footer>
+    <div class="footer">
+      <p>© 2025 Nexus. Todos los derechos reservados.</p>
+      <div class="social-icons">
+        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+      </div>
+
+      <div class="pg_error">
+        <a href="Error-404.html">Error 404</a>
+        <a href="error.500.html">Error 500</a>
+      </div>
+  </footer>
+</body>
+
+</html>
